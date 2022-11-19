8e1fbf4c5aa785d663e8380b2457fa75405365c7
Matthew Johnston <mattdjohnston92@gmail.com>
2022-11-18 22:17:21 -0400

    Add updated study notes and files.

--- course/part3/backend/index.js
+++ course/part3/backend/index.js
@@ -1,32 +1,16 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const app = require('./app');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
-const notesRouter = require('./controllers/notes');
-const middleware = require('./utils/middleware');
-const mongoose = require('mongoose');
-const app = express();
+const http = require('http');
 
-logger.info('connecting to', config.MONGODB_URI);
+const server = http.createServer(app);
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
-
-app.use(cors());
-app.use(express.static('build'));
-app.use(express.json());
-app.use(middleware.requestLogger);
-app.use('/api/notes', notesRouter);
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
-
-const PORT = config.PORT;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
+
+// const PORT = config.PORT;
+// app.listen(PORT, () => {
+//   logger.info(`Server running on port ${PORT}`);
+// });
