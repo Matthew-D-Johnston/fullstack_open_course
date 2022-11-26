46504a478fe01e913c1a03eb99067567ed291ccb
Matthew-D-Johnston <49563465+Matthew-D-Johnston@users.noreply.github.com>
2022-11-26 13:13:55 -0400

    Add study files.

--- course/routed-anecdotes/src/App.js
+++ course/routed-anecdotes/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useField } from './hooks/index'
 import {
   BrowserRouter as Router,
   Routes,
@@ -69,28 +70,32 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content');
+  const author = useField('author');
+  const info = useField('info');
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
-    setContent('')
-    setAuthor('')
-    setInfo('')
-    props.setNotification(`a new anecdote ${content} created!`)
+    props.setNotification(`a new anecdote ${content.value} created!`)
     setTimeout(() => {
       props.setNotification('')
     }, 5000)
+  }
+
+  const handleReset = (event) => {
+    event.preventDefault();
+    content.clearvalue();
+    author.clearvalue();
+    info.clearvalue();
   }
 
   return (
@@ -99,17 +104,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} clearvalue=''/>
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} clearvalue=''/>
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} clearvalue=''/>
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
